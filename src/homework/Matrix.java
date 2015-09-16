8cd061af9ced41639639edf65d1823beca274a50
Dmitrii Semenov <dmitrii_semenov@epam.com>
2015-09-16 18:19:00 +0300

    extract printMatrix method

--- src/homework/Matrix.java
+++ src/homework/Matrix.java
@@ -11,30 +11,35 @@ package homework;
 public class Matrix {
 	public static void main(String[] args) {
 		int[][] matrix = new int[30][20];
-		boolean isPrintZeroes = true;						//change it into false if you hate zeroes
 		for (int i = 0; i < matrix.length; i++) {
-			int zerocounter = 0;
-			OUTER:
 			for (int j = 0; j < matrix[i].length; j++) {
 				if((i % (matrix[i].length) == j)			//main diagonal and below
 						||((i+j+1)%matrix[i].length == 0)	//reflection and below
 						){
 					matrix[i][j]=1;
-					System.out.print(matrix[i][j]+ " ");
-//					break OUTER; // use 'break' instead of 'continue' to see some different digital painting
-					continue OUTER;
 				}
-				else {
-					if(isPrintZeroes){
-						System.out.print("0 ");
-					}
-					else{
-						System.out.print("  ");
-					}
+			}
+		}
+		printMatrix(matrix, "1 ", "  ");
+	}
+	
+	private static void printMatrix(int[][] matrix, String label1, String label0){
+		for (int i = 0; i < matrix.length; i++) {
+			int zerocounter = 0;
+			OUTER:
+			for (int j = 0; j < matrix[i].length; j++) {
+				if(matrix[i][j] == 1){
+					System.out.print(label1);
+					continue OUTER;//comment this block to get mistake in zeroes counting
+				}
+				else{
+					System.out.print(label0);
 				}
 				zerocounter++;
+				
 			}
-		System.out.println("    count of zeroes in line: " + zerocounter);	
+			System.out.println("    count of zeroes in vector: " + zerocounter);
 		}
+		
 	}
 }
