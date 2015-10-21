bc3359fdae10bddc789be7b2366cf457fdca2789
Dmitrii Semenov <dmitrii_semenov@epam.com>
2015-10-21 16:18:28 +0300

    Hello

--- src/start/Hello.java
+++ src/start/Hello.java
@@ -7,26 +7,31 @@ import homework.Sorting;
 public class Hello {
 
 	public static void main(String[] args) {
-		Sorting sorting = new Sorting();
-		System.out.println("Hello, java!!!");
-		System.out.println();
-		int[] arr = {4,2,9,0,2,1,65,5,8,6,76,7,3};
+//		Sorting sorting = new Sorting();
+//		System.out.println("Hello, java!!!");
+//		System.out.println();
+//		int[] arr = {4,2,9,0,2,1,65,5,8,6,76,7,3};
+//		
+//		System.out.println("Source array: "+ Arrays.toString(arr));
+//		System.out.println("Bubble-sorted array : "+ Arrays.toString(sorting.bubbleSort(arr.clone())));
+//		System.out.println();
+//		
+//		System.out.println("Source array: "+ Arrays.toString(arr));
+//		System.out.println("Silly-sorted  array : "+ Arrays.toString(sorting.sillySort(arr.clone())));
+//		System.out.println();
+//		
+//		System.out.println("Source array: "+ Arrays.toString(arr));
+//		System.out.println("Shaker-sorted array : "+ Arrays.toString(sorting.shakerSort(arr.clone())));
+//		System.out.println();
+//		
+//		System.out.println("Source array: "+ Arrays.toString(arr));
+//		System.out.println("EvenOdd-sorted array: "+ Arrays.toString(sorting.evenOddSort(arr.clone())));
+//		System.out.println();
 		
-		System.out.println("Source array: "+ Arrays.toString(arr));
-		System.out.println("Bubble-sorted array : "+ Arrays.toString(sorting.bubbleSort(arr.clone())));
-		System.out.println();
-		
-		System.out.println("Source array: "+ Arrays.toString(arr));
-		System.out.println("Silly-sorted  array : "+ Arrays.toString(sorting.sillySort(arr.clone())));
-		System.out.println();
-		
-		System.out.println("Source array: "+ Arrays.toString(arr));
-		System.out.println("Shaker-sorted array : "+ Arrays.toString(sorting.shakerSort(arr.clone())));
-		System.out.println();
-		
-		System.out.println("Source array: "+ Arrays.toString(arr));
-		System.out.println("EvenOdd-sorted array: "+ Arrays.toString(sorting.evenOddSort(arr.clone())));
-		System.out.println();
+		String[] testLibrariesNames = new String[]{"test03_6", "test03_3", "test03_2", "test03_4", "test03_5", "test03_1"};
+		for (String string : testLibrariesNames) {
+			System.out.println(string);
+		}
 	}
 
 }
